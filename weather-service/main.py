e48840d47facc6cc96a753842c6a9686361eaa63
Zach Chehayeb <zachchehayeb@gmail.com>
2017-12-20 20:47:46 -0800

    Added threading ability so calls are made in parallel to speed up response time. DarkSky only allows for calling a single day in the past.

--- weather-service/main.py
+++ weather-service/main.py
@@ -15,35 +15,46 @@ from bottle import Bottle
 import urllib2 
 import json
 from time import time
+import threading
 
 app = Bottle()
 
-@app.get('/getweather/<lat>,<long>')
-def get_weather2(lat, long):
-	darksky_url = "https://api.darksky.net/forecast"
-	darksky_key = "8bb170dfde985aeb11bc1e26ba2491e9"
-	current_time = int(time())
-	excludes = "currently,minutely,hourly,alerts,flags"
-	day_lenth = 86400
-	day = current_time
-	weather_list = []
+######################################
+# Global Vars
+######################################
+#Dark Sky URL Params
+darksky_url = "https://api.darksky.net/forecast"
+darksky_key = "8bb170dfde985aeb11bc1e26ba2491e9"
+excludes = "currently,minutely,hourly,alerts,flags"
 
-	for each in range(0, 7):	
-		weather_url ="{0}/{1}/{2},{3},{4}?exclude={5}".format(darksky_url, darksky_key, lat, long, day, excludes)
-		darksky_call = urllib2.urlopen(weather_url)
-		
-		#Basic status code check to ensure we get back data
-		status_code = darksky_call.getcode()
-		if status_code == 200:
-			weather_json = json.load(darksky_call)
-			day_weather = weather_json
-			weather_list.append({"status": status_code, "result": day_weather, "time": day})
-		else:
-			weather_list.append({"status": status_code, "result": None, "time": day})
+#Current Time
+current_time = int(time())
+day_lenth = 86400
+
+def get_days_weather(each, weather_list, lat, long):
+	day = current_time - (day_lenth * each)
+	weather_url ="{0}/{1}/{2},{3},{4}?exclude={5}".format(darksky_url, darksky_key, lat, long, day, excludes)
+	darksky_call = urllib2.urlopen(weather_url)
+	status_code = darksky_call.getcode()
+	if status_code == 200:
+		weather_json = json.load(darksky_call)
+		day_weather = weather_json 
+		weather_list.append({"status": status_code, "result": day_weather, "time": day})
+	else:
+		weather_list.append({"status": status_code, "result": None, "time": day})
 	
-		day -= day_lenth
-	
+@app.route('/getweather/<lat>,<long>')
+def get_weather(lat, long):
+	weather_list = []
+	jobs = []
+	for x in range(7):
+		process = threading.Thread(target = get_days_weather, args = (x, weather_list, lat, long))
+		jobs.append(process)
+		process.start()
+	for proc in jobs:
+		proc.join()
 	weather_full_json = json.dumps(weather_list)
 	return weather_full_json	
 
 app.run(host='192.168.1.210', port = 8000)
+	
